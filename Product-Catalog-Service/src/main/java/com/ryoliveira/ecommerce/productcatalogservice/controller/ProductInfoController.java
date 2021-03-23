957e93f4fb4125108f074f55d32afb3f636781d0
Ryan <windsofryannnn@yahoo.com>
2021-03-22 23:22:37 -0700

    Added more details to controller http annotations

--- Product-Catalog-Service/src/main/java/com/ryoliveira/ecommerce/productcatalogservice/controller/ProductInfoController.java
+++ Product-Catalog-Service/src/main/java/com/ryoliveira/ecommerce/productcatalogservice/controller/ProductInfoController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,44 +34,42 @@ public class ProductInfoController {
 	@Autowired
 	private ProductInfoService prodInfoService;
 
-	@PostMapping(path="/productInfo", consumes= {"multipart/form-data"})
+	@PostMapping(path="/productInfo", consumes= {MediaType.MULTIPART_FORM_DATA_VALUE})
 	private ResponseEntity<ProductInfo> saveProductInfo(@RequestPart("productInfo") String productInfoJsonString,
 			@RequestPart(name="mainProductImage", required=true) MultipartFile mainProductImage, 
 			@RequestPart(name="additionalFiles[]", required=false) List<MultipartFile> additionalFiles) {
 		return new ResponseEntity<>(prodInfoService.saveProductInfo(productInfoJsonString, additionalFiles, mainProductImage), HttpStatus.OK);
 	}
 
-	
-	//TODO ImagesToDelete is not being consumed properly
-	@PutMapping(path="/productInfo", consumes= {"multipart/form-data"})
+	@PutMapping(path="/productInfo", consumes={MediaType.MULTIPART_FORM_DATA_VALUE})
 	private ResponseEntity<ProductInfo> updateProductInfo(@RequestPart("productInfo") String updatedProdInfoJsonString, 
 			@RequestPart(name="mainProductImage", required=false) MultipartFile mainProductImage,
 			@RequestPart(name="additionalImageFiles[]", required=false) List<MultipartFile> additionalImageFiles) {
 		return new ResponseEntity<>(prodInfoService.updateProductInfo(updatedProdInfoJsonString, mainProductImage, additionalImageFiles), HttpStatus.OK);
 	}
 
-	@GetMapping("/productInfo/{prodId}")
+	@GetMapping(path="/productInfo/{prodId}")
 	private ResponseEntity<ProductInfo> getProductInfo(@PathVariable("prodId") int prodId) {
 		return new ResponseEntity<>(prodInfoService.getProductInfo(prodId), HttpStatus.OK);
 	}
 
-	@DeleteMapping("/productInfo/{prodId}")
+	@DeleteMapping(path="/productInfo/{prodId}")
 	private ResponseEntity<String> deleteProduct(@PathVariable("prodId") int prodId) {
 		prodInfoService.deleteProduct(prodId);
 		return new ResponseEntity<>("Product with id: " + prodId + " successfully deleted", HttpStatus.OK);
 	}
 
-	@GetMapping("/products")
+	@GetMapping(path="/products")
 	private ResponseEntity<ProductInfoList> getAllProducts() {
 		return new ResponseEntity<>(prodInfoService.getAllProducts(), HttpStatus.OK);
 	}
 
-	@GetMapping("/products/{categoryId}")
+	@GetMapping(path="/products/{categoryId}")
 	private ResponseEntity<ProductInfoList> getAllProductsByCategoryId(@PathVariable("categoryId") int categoryId) {
 		return new ResponseEntity<>(prodInfoService.getAllProductsByCategoryId(categoryId), HttpStatus.OK);
 	}
 
-	@GetMapping("/categories")
+	@GetMapping(path="/categories")
 	private ResponseEntity<CategoryList> getAllCategories() {
 		return new ResponseEntity<>(prodInfoService.getAllCategories(), HttpStatus.OK);
 	}
