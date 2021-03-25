29af01b2d160a7feebbdfb2c7f2e28cb57f2c664
Ryan <windsofryannn@yahoo.com>
2021-03-24 22:00:54 -0700

    Cleaned up code in image-service controller

--- Image-Service/src/main/java/com/ryoliveira/ecommerce/image/controller/ImageController.java
+++ Image-Service/src/main/java/com/ryoliveira/ecommerce/image/controller/ImageController.java
@@ -1,68 +1,58 @@
 package com.ryoliveira.ecommerce.image.controller;
 
-import java.util.List;
-
+import com.ryoliveira.ecommerce.image.model.Image;
+import com.ryoliveira.ecommerce.image.service.ImageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.ryoliveira.ecommerce.image.model.Image;
-import com.ryoliveira.ecommerce.image.service.ImageService;
+import java.util.List;
 
 @RestController
 public class ImageController {
 
-	Logger LOGGER = LoggerFactory.getLogger(ImageController.class);
-	
-	@Autowired
-	private ImageService imageService;
-	
-	@PostMapping("/image")
-	private ResponseEntity<Image> saveImage(@RequestBody Image img) {
-		return new ResponseEntity<Image>(imageService.saveImage(img), HttpStatus.OK);
-	}
-	
-	@GetMapping("/image/{imageId}")
-	private ResponseEntity<Image> getImage(@PathVariable("imageId") int imageId) {
-		return new ResponseEntity<Image>(imageService.getImage(imageId), HttpStatus.OK);
-	}
-	
-	@PutMapping("/image/{productId}")
-	private ResponseEntity<Image> updateProductMainImage(@RequestBody Image updatedImg, @PathVariable("productId") int productId) {
-		return new ResponseEntity<Image>(imageService.updateImage(updatedImg, productId), HttpStatus.OK);
-	}
-	
-	@DeleteMapping("/image/{imageId}")
-	private ResponseEntity<String> deleteImage(@PathVariable("imageId") int imageId){
-		imageService.deleteImage(imageId);
-		return new ResponseEntity<String>("Image with id: " + imageId + " has been deleted", HttpStatus.OK);
-	}
-	
-	@DeleteMapping("/images/{productId}")
-	private ResponseEntity<String> deleteImagesWithProductId(@PathVariable("productId") int productId){
-		imageService.deleteImagesWithProductId(productId);
-		return new ResponseEntity<String>("Images with product id: " + productId + " have been deleted", HttpStatus.OK);
-	}
-	
-	@GetMapping("/images/{productId}")
-	private ResponseEntity<List<Image>> getProductImages(@PathVariable("productId") int productId, @RequestParam("productMainImageOnly") boolean productMainImageOnly){
-		return new ResponseEntity<>(imageService.getProductImages(productId, productMainImageOnly), HttpStatus.OK);
-	}
-	
-	@PutMapping(path="/images")
-	private ResponseEntity<String> deleteImagesById(@RequestBody List<Integer> imageIdsToDelete){
-		imageService.deleteImagesByIds(imageIdsToDelete);
-		return new ResponseEntity<String>("", HttpStatus.OK);
-	}
+    @Autowired
+    private ImageService imageService;
+
+    @PostMapping(path = "/image")
+    private ResponseEntity<Image> saveImage(@RequestBody Image img) {
+        return new ResponseEntity<Image>(imageService.saveImage(img), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/image/{imageId}")
+    private ResponseEntity<Image> getImage(@PathVariable("imageId") int imageId) {
+        return new ResponseEntity<Image>(imageService.getImage(imageId), HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/image/{productId}")
+    private ResponseEntity<Image> updateProductMainImage(@RequestBody Image updatedImg, @PathVariable("productId") int productId) {
+        return new ResponseEntity<Image>(imageService.updateImage(updatedImg, productId), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/image/{imageId}")
+    private ResponseEntity<String> deleteImage(@PathVariable("imageId") int imageId) {
+        imageService.deleteImage(imageId);
+        return new ResponseEntity<String>("Image with id: " + imageId + " has been deleted", HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/images/{productId}")
+    private ResponseEntity<String> deleteImagesWithProductId(@PathVariable("productId") int productId) {
+        imageService.deleteImagesWithProductId(productId);
+        return new ResponseEntity<String>("Images with product id: " + productId + " have been deleted", HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/images/{productId}")
+    private ResponseEntity<List<Image>> getProductImages(@PathVariable("productId") int productId, @RequestParam("productMainImageOnly") boolean productMainImageOnly) {
+        return new ResponseEntity<>(imageService.getProductImages(productId, productMainImageOnly), HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/images")
+    private ResponseEntity<String> deleteImagesById(@RequestBody List<Integer> imageIdsToDelete) {
+        imageService.deleteImagesByIds(imageIdsToDelete);
+        return new ResponseEntity<String>("", HttpStatus.OK);
+    }
 
 }
