a720a3307371bb1cd439d4a02f56319b1ca3da0b
Ryan <windsofryannn@yahoo.com>
2021-03-24 20:39:50 -0700

    Cleaned up code in product-service controller

--- Product-Service/src/main/java/com/ryoliveira/ecommerce/productservice/controller/ProductController.java
+++ Product-Service/src/main/java/com/ryoliveira/ecommerce/productservice/controller/ProductController.java
@@ -1,60 +1,49 @@
 package com.ryoliveira.ecommerce.productservice.controller;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.ryoliveira.ecommerce.productservice.model.Product;
+import com.ryoliveira.ecommerce.productservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.ryoliveira.ecommerce.productservice.model.Product;
-import com.ryoliveira.ecommerce.productservice.service.ProductService;
+import java.util.List;
+
 
 @RestController
 public class ProductController {
 
-	Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+    @Autowired
+    private ProductService prodService;
 
-	@Autowired
-	private ProductService prodService;
+    @PostMapping(path = "/product")
+    private ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+        return new ResponseEntity<>(prodService.saveProduct(product), HttpStatus.OK);
+    }
 
-	@PostMapping("/product")
-	private ResponseEntity<Product> saveProduct(@RequestBody Product product) {
-		return new ResponseEntity<>(prodService.saveProduct(product), HttpStatus.OK);
-	}
+    @PutMapping(path = "/product")
+    private ResponseEntity<Product> updateProduct(@RequestBody Product product) {
+        return new ResponseEntity<>(prodService.updateProduct(product), HttpStatus.OK);
+    }
 
-	@PutMapping("/product")
-	private ResponseEntity<Product> updateProduct(@RequestBody Product product) {
-		return new ResponseEntity<>(prodService.updateProduct(product), HttpStatus.OK);
-	}
+    @GetMapping(path = "/product/{prodId}")
+    private ResponseEntity<Product> getProduct(@PathVariable("prodId") int prodId) {
+        return new ResponseEntity<>(prodService.findProductById(prodId), HttpStatus.OK);
+    }
 
-	@GetMapping("/product/{prodId}")
-	private ResponseEntity<Product> getProduct(@PathVariable("prodId") int prodId) {
-		return new ResponseEntity<>(prodService.findProductById(prodId), HttpStatus.OK);
-	}
+    @DeleteMapping(path = "/product/{prodId}")
+    private ResponseEntity<String> removeProduct(@PathVariable("prodId") int prodId) {
+        prodService.removeProduct(prodId);
+        return new ResponseEntity<>("Product removed with id: " + prodId, HttpStatus.OK);
+    }
 
-	@DeleteMapping("/product/{prodId}")
-	private ResponseEntity<String> removeProduct(@PathVariable("prodId") int prodId) {
-		prodService.removeProduct(prodId);
-		return new ResponseEntity<>("Product removed with id: " + prodId, HttpStatus.OK);
-	}
+    @GetMapping(path = "/products")
+    private List<Product> getAllProducts() {
+        return prodService.findAll();
+    }
 
-	@GetMapping("/products")
-	private List<Product> getAllProducts() {
-		return prodService.findAll();
-	}
-
-	@GetMapping("/products/{categoryId}")
-	private List<Product> getAllProductsByCategoryId(@PathVariable("categoryId") int categoryId) {
-		return prodService.findAllByCategoryId(categoryId);
-	}
-
+    @GetMapping(path = "/products/{categoryId}")
+    private List<Product> getAllProductsByCategoryId(@PathVariable("categoryId") int categoryId) {
+        return prodService.findAllByCategoryId(categoryId);
+    }
 }
